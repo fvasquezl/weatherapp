3a1c714cbce69ec708ada7157c43dc92f24f2ef3
fvasquezl <vasquez.limon@gmail.com>
2023-07-31 23:34:14 -0700

    first commit

---
+++ app/search/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { useState } from 'react'
+import SearchInput from '../components/SearchInput'
+import WeatherForecast from '../components/WeatherForecast'
+import { CurrentWeather as CurrentWeatherType, Forecast } from '../models/wheather'
+import fetchWeatherData from '../services/wheather'
+
+
+const SearchPage: React.FC = () => {
+    const [forecast, setForecast] = useState<Forecast[] | null>(null)
+    const handleSearch = async (city: string) => {
+        try {
+            const { forecast } = await fetchWeatherData(city)
+            setForecast(forecast)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return (
+        <div>
+            <SearchInput onSearch={handleSearch} />
+            {forecast && <WeatherForecast forecast={forecast} />}
+        </div>
+    )
+}
+
+export default SearchPage
---
+++ app/services/wheather.ts
@@ -0,0 +1,43 @@
+import { CurrentWeather, Forecast } from "../models/wheather"
+
+
+const apiKey = "0050d91bae2b0da38504bc3ec9ac7663"
+const baseUrl = "https://api.openweathermap.org/data/2.5/"
+
+
+const fetchWeatherData = async (city: string): Promise<{ currentWeather: CurrentWeather, forecast: Forecast[] }> => {
+    const currentWeatherResponse = await fetch(`${baseUrl}/weather?q=${city}&units=metric&appid=${apiKey}`);
+    const currentWeatherData = await currentWeatherResponse.json()
+
+    if (currentWeatherData.cod !== 200) {
+        throw new Error(currentWeatherData.message)
+    }
+
+    const forecastForecast = await fetch(`${baseUrl}/forecast?q=${city}&units=metric&appid=${apiKey}`)
+    const forecastData = await forecastForecast.json()
+
+    if (forecastData.cod !== 200) {
+        throw new Error(forecastData.message)
+    }
+
+    const currentWeather: CurrentWeather = {
+        city: currentWeatherData.name,
+        country: currentWeatherData.sys.country,
+        temperature: currentWeatherData.main.temp,
+        description: currentWeatherData.wheather[0].description,
+        icon: currentWeatherData.weather[0].icon
+    }
+
+    const forecast: Forecast[] = forecastData.list
+        .filter((item: any, index: number) => index % 8 === 0)
+        .slice(0, 5)
+        .map((item: any) => ({
+            date: new Date(item.dt_txt),
+            temperature: item.main.temperature,
+            description: currentWeatherData.wheather[0].description,
+            icon: currentWeatherData.weather[0].icon
+        }))
+
+    return { currentWeather, forecast }
+}
+export default fetchWeatherData
